a6e825b84eb837c9be0e970529250389567e9402
Jakub Bieniek <bienio59@gmail.com>
2025-10-07 18:04:25 +0200

    add initial database setup with Alembic migrations and Docker configuration changes

---
+++ backend/app/core/config.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
+from pydantic import AnyHttpUrl, EmailStr, validator
+from pydantic_settings import BaseSettings
+from alembic import context
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+sys.path.append(BASE_DIR)
+
+class Settings(BaseSettings):
+    DATABASE_URL: str  = os.getenv('DATABASE_URL')
+
+    class Config:
+        env_file = ".env"
+        extra = "allow"
+
+settings = Settings()
+
+if __name__ == '__main__':
+    print(settings.DATABASE_URL)
