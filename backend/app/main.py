4d290c8db3dd0c4df48ec3343a6ba39a8686ac05
Jakub Bieniek <bienio59@gmail.com>
2025-10-11 13:43:55 +0200

    CORS add

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from .routers import auth, users,admin,notes,saved_notes,note_ratings,note_comments
 from .db.init_db import init_db
 from .db.base import SessionLocal
+from fastapi.middleware.cors import CORSMiddleware
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,6 +26,19 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="FastAPI Auth",
               lifespan=lifespan)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
