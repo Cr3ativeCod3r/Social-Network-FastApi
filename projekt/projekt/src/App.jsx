61e6d9405a8f6f8a65251a6582461a8225bb739a
kamil <kamil@MacBook-Air-kamil.local>
2025-10-27 18:17:13 +0100

    update

---
+++ projekt/projekt/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import UserManagementSketch from "./screens2";
+import StudyShareSketch from "./screens1";
+import StudyShareFeaturesSketch from "./screens3";
+function App() {
+  return (
+    <>
+      <StudyShareSketch />
+      <UserManagementSketch />
+      <StudyShareFeaturesSketch/>
+    </>
+  );
+}
+
+export default App;
